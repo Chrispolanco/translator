a829a110217d9b28b7562e63285d0e944964474f
Chrispolanco <chrispolanco@sbcglobal.net>
2022-01-02 00:10:19 -0800

    fixed index.html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./styles.css";
 import React from "react";
+import Field from './components/field'; 
+import Languages from './components/languages'; 
+import Translate from './components/translate'; 
 
 export default function App() {
   return (
-    <div>
-      <h1 className="title">Hi there!</h1>
+    <div> 
+      <Field/> 
+      <Languages/> 
+      <Translate/> 
     </div>
   );
 }
